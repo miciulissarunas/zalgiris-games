7f755cb469362149718146279af79f19fcebbb17
miciulissarunas <miciulis.sarunas@gmail.com>
2026-04-13 12:31:00 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -2,17 +2,44 @@ import fetch from "node-fetch";
 import fs from "fs";
 
 async function getNextGame() {
-  const res = await fetch("https://site.api.espn.com/apis/site/v2/sports/basketball/euroleague/teams/2674/schedule");
+  const url = "https://site.api.espn.com/apis/site/v2/sports/basketball/euroleague/teams/2674/schedule";
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`HTTP error ${res.status}`);
+  }
+
   const data = await res.json();
+
+  console.log("Top-level keys:", Object.keys(data));
+
+  const events = Array.isArray(data.events)
+    ? data.events
+    : Array.isArray(data?.team?.nextEvent)
+    ? data.team.nextEvent
+    : [];
+
+  if (!events.length) {
+    console.log("Full response:", JSON.stringify(data, null, 2));
+    throw new Error("No events array found in API response");
+  }
 
   const now = new Date();
 
-  const games = data.events
-    .map(game => ({
-      date: new Date(game.date),
-      home: game.competitions[0].competitors[0].team.displayName,
-      away: game.competitions[0].competitors[1].team.displayName
-    }))
+  const games = events
+    .map(game => {
+      const comp = game?.competitions?.[0];
+      const competitors = comp?.competitors || [];
+
+      if (competitors.length < 2) return null;
+
+      return {
+        date: new Date(game.date),
+        home: competitors[0]?.team?.displayName || "Unknown",
+        away: competitors[1]?.team?.displayName || "Unknown"
+      };
+    })
+    .filter(Boolean)
     .filter(game => game.date > now)
     .sort((a, b) => a.date - b.date);
 
